a910ca18bc41081770ec6b6c551e9d8ee01a12f8
Diego <diego_tapia_millan@yahoo.es>
2022-08-08 12:22:47 +0200

    basic structure created

---
+++ front/src/components/header/Header.js
@@ -0,0 +1,6 @@
+import "./Header.module.css";
+
+export const Header = () => {
+  return <h2>header</h2>
+
+};
---
+++ front/src/pages/notFound/NotFound.js
@@ -0,0 +1,9 @@
+import classes from "./NotFound.module.css"
+
+export const NotFound = () => {
+  return (
+    <div className={classes.container}>
+      <h1>Error: Not Found</h1>
+    </div>
+  );
+};
